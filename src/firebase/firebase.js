112a8b30a771a1bfae4ab6bd021f7f7c66c44006
lemarchesi09 <lemarchesi9@gmail.com>
2022-10-29 16:00:28 -0300

    probando

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkuEuuCwjVZ9UgPIQ0SWjCpG_P2DAMKPI",
+  authDomain: "checkpoint4-e-commerce.firebaseapp.com",
+  projectId: "checkpoint4-e-commerce",
+  storageBucket: "checkpoint4-e-commerce.appspot.com",
+  messagingSenderId: "395730090398",
+  appId: "1:395730090398:web:05c2012865f60e28bebe8f",
+  measurementId: "G-80VY2L4DVH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
