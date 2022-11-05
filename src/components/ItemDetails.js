5c0740b622097e49a44b381d097dca4e3d94e8a1
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-11-04 22:47:35 -0300

    finished styles in the itemDetails component

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import '../styles/itemDetails.css'
+import "../styles/itemDetails.css";
 const ItemDetails = () => {
   const { id } = useParams();
   const [item, setItem] = useState({});
-  const { title, price, description, image } = item;
+  const { title, price, description, image,category } = item;
+  console.log(category);
 
   const getDataItem = async () => {
     const url = `https://fakestoreapi.com/products/${id}`;
@@ -17,30 +18,29 @@ const ItemDetails = () => {
     getDataItem();
   }, []);
   return (
-    <div>
-      <div className="itemsDetailsContainer container ">
-        <div className="itemsDetailsContainer__data d-flex ">
-          <div className="itemsDetailsContainer__data__img">
-            <img src={image} alt="" />
-          </div>
-          <div className="itemsDetailsContainer__data ">
-          <h1>{title}</h1>
-          <h2>{description}</h2>
-          <p> ${price}</p>
-
-          </div>
-          <div className="itemsDetailsContainer__data__img">
-            <input type="number" />
-            <button className="btn btn-primary">Buy now</button>
-            <button className="btn btn-primary" >Add to cart </button>
-          </div>
-     
-        </div>
+    <div className="p-4" >
+     <div class="card  col-md-4 w-100  " >
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={image} class="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div class="col-md-6">
+      <div class="card-body d-flex  flex-column justify-content-center">
+      <p class="card-text"><span>{category}</span>.</p>
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{description}.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
-
-  
+      <div className="card-buttons row-md-2 d-flex justify-content-around">
+      <input type="number"  style={{width:'70px'}}/>
+      <button className="btn btn-primary ">Buy now</button>
+      <button className="btn btn-primary">Add now </button>
+    </div>
+    </div>
+   
   </div>
-
+</div>
+    </div>
   );
 };
 
