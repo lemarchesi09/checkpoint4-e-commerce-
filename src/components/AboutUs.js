390d173f388dde790ff6c29a985bc17ffdc389dc
[kucki90] <[c.kuckiewicz@hotmail.com]>
2022-11-07 20:55:14 -0300

    contenido de cards definido

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,15 +1,23 @@
 import "../styles/about.css";
 import React from "react";
-import Contactanos from "./Contactanos.jpg";
+import { Link } from "react-router-dom";
+/* import Contactanos from "./Contactanos.jpg"; */
 import user1 from "./user1.jpeg";
 import user2 from "./user2.jpeg";
 import user3 from "./user3.jpeg";
 import user4 from "./user4.jpeg";
 import Card from 'react-bootstrap/Card';
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFillGeoAltFill } from "react-icons/bs";
+import { BsEnvelope } from "react-icons/bs";
+import { BsFileEarmark } from "react-icons/bs";
+import { BsPhone } from "react-icons/bs";
+/* import { Mailto } from "react-mailto"; */
 
 export const AboutUs = () => (
   <div className="about">
-    <img src={Contactanos}/>
+    {/* <img src={Contactanos}/> */}
     <h2>Te contamos un poco sobre nosotros...</h2>
     <p>
       Somos un grupo de desarrolladores que pudo encontrar un maraviloso ritmo y coordinación a la hora de trabajar.
@@ -20,50 +28,55 @@ export const AboutUs = () => (
     <Card style={{ width: '15rem' }}>
       <Card.Img variant="top" src={user1} />
       <Card.Body>
-        <Card.Title>Florencia Miguez Oliverio</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <Card.Title style={{ height: '3rem' }}>Florencia Miguez Oliverio</Card.Title>
+        <Card.Text className="row">
+        <Link to="https://github.com/" className=""> <BsGithub/> Github </Link>
+        <Link to="https://www.linkedin.com/in/" className=""> <BsLinkedin/> LinkedIn </Link>
+        <div><BsFillGeoAltFill/> Ubicación</div> 
         </Card.Text>
       </Card.Body>
     </Card>
     <Card style={{ width: '15rem' }}>
       <Card.Img variant="top" src={user2} />
       <Card.Body>
-        <Card.Title>Ignacio Benitez</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <Card.Title style={{ height: '3rem' }}>Ignacio Benitez</Card.Title>
+        <Card.Text className="row">
+        <Link to="https://github.com/" className=""> <BsGithub/> Github </Link>
+        <Link to="https://www.linkedin.com/in/" className=""> <BsLinkedin/> LinkedIn </Link>
+        <div><BsFillGeoAltFill/> Ubicación</div>
         </Card.Text>
       </Card.Body>
     </Card>
     <Card style={{ width: '15rem' }}>
       <Card.Img variant="top" src={user3} />
       <Card.Body>
-        <Card.Title>Enzo Marchesi</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <Card.Title style={{ height: '3rem' }}>Enzo Marchesi</Card.Title>
+        <Card.Text className="row">
+        <Link to="https://github.com/" className=""> <BsGithub/> Github </Link>
+        <Link to="https://www.linkedin.com/in/" className=""> <BsLinkedin/> LinkedIn </Link>
+        <div><BsFillGeoAltFill/> Ubicación</div>
         </Card.Text>
       </Card.Body>
     </Card>
     <Card style={{ width: '15rem' }}>
       <Card.Img variant="top" src={user4} />
       <Card.Body>
-        <Card.Title>Ian Noreiko</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <Card.Title style={{ height: '3rem' }}>Ian Noreiko</Card.Title>
+        <Card.Text className="row">
+        <Link to="https://github.com/" className=""> <BsGithub/> Github </Link>
+        <Link to="https://www.linkedin.com/in/" className=""> <BsLinkedin/> LinkedIn </Link>
+        <div><BsFillGeoAltFill/> Ubicación</div>
         </Card.Text>
       </Card.Body>
     </Card>
     <Card style={{ width: '15rem' }}>
       <Card.Img variant="top" src={user1} />
       <Card.Body>
-        <Card.Title>Cecilia Kuckiewicz</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <Card.Title style={{ height: '3rem' }}>Cecilia Kuckiewicz</Card.Title>
+        <Card.Text className="row">
+        <Link to="https://github.com/kucki90" className=""> <BsGithub/> Github </Link>
+        <Link to="https://www.linkedin.com/in/cecilia-kuckiewicz/" className=""> <BsLinkedin/> LinkedIn </Link>
+        <div><BsFillGeoAltFill/> Vicente López - Buenos Aires</div>
         </Card.Text>
       </Card.Body>
     </Card>
