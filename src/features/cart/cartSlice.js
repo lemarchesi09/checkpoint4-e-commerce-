aa91c492d003eaedb1a48dddc217e36db1a4ea11
ianbpn <ianb.782@live.com>
2022-11-08 16:27:11 -0300

    cart starting

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Action } from "@remix-run/router";
+
+const initialState  = [];
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers:{
+        addCart: (state, action)=> {
+            state.push(action.payload);
+        },
+        deleteProduct: (state,action)=>{
+            return state.filter((product) => product.id !== action.payload);
+        }
+    }
+})
