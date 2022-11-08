e02342b5abbab4046d27bd34a36b8854cb67da9c
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-11-08 19:22:15 -0300

    added product at global state

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,63 +1,80 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-//import { Rating } from 'react-simple-star-rating'
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../features/product/productsSlice";
 import "../styles/itemDetails.css";
+
 const ItemDetails = () => {
   const { id } = useParams();
   const [item, setItem] = useState({});
-  const [quantity,setQuantity] =useState(0)
-  const [count,setCount] =useState(1)
-  const { title, price, description, image, category,rate } = item;
-
+  // state for calculate quantity*price
+  const [quantity, setQuantity] = useState(0);
+  // state for the input
+  const [count, setCount] = useState(1);
+  const { title, price, description, image, category, rate } = item;
+  const dispatch= useDispatch()
+  const navigate = useNavigate();
 
   const getDataItem = async () => {
     const url = `https://fakestoreapi.com/products/${id}`;
-    console.log(url);
     const data = await fetch(url);
     const res = await data.json();
     setItem(res);
   };
-  const getQuantity=(e)=>{
-      setQuantity (Number(e.target.value))
-      setCount(Number(e.target.value))
-
-  
-    console.log(quantity);
+  const getQuantity = (e) => {
+    setQuantity(Number(e.target.value));
+    setCount(Number(e.target.value));
+  };
+  const addToCart=()=>{
+dispatch(addProduct(item))
+navigate('/')
+alert("producto cargado re piola ameo 🤙")
   }
   useEffect(() => {
     getDataItem();
   }, []);
   return (
     <div className="p-4">
-      <div class="card  col-md-4 w-100  ">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src={image} class="img-fluid rounded-start" alt="..." />
+      <div className="card  col-md-4 w-100  ">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={image} className="img-fluid rounded-start" alt="..." />
           </div>
-          <div class="col-md-6">
-            <div class="card-body d-flex  flex-column justify-content-center">
-              <p class="card-text">
+          <div className="col-md-6">
+            <div className="card-body d-flex  flex-column justify-content-center">
+              <p className="card-text">
                 <span>{category}</span>.
               </p>
-              <h5 class="card-title">{title}</h5>
-              <p class="card-text">{description}.</p>
-              
-           {quantity>1? <p class="card-text">${price*quantity} </p> : <p class="card-text">${price}</p> }
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}.</p>
+
+              {quantity > 1 ? (
+                <p className="card-text">${price * quantity} </p>
+              ) : (
+                <p className="card-text">${price}</p>
+              )}
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
               </p>
             </div>
             <div className=" row-md-2 d-flex justify-content-around">
-              <div className="card_input_count" >
-              <input type="number" min={1} max={10} value={count} onChange={getQuantity} style={{ width: "70px" }} />
+              <div className="card_input_count">
+                <input
+                  type="number"
+                  min={1}
+                  max={10}
+                  value={count}
+                  onChange={getQuantity}
+                  style={{ width: "70px" }}
+                />
               </div>
-         
+
               <div className="card-buttons d-flex  col-md-6 gap-4  ">
                 <div className="card-button_buy">
                   <button className="btn btn-primary ">Buy now</button>
                 </div>
                 <div className="card-button_Add ">
-                  <button className="btn btn-primary">Add now </button>
+                  <button className="btn btn-primary" onClick={addToCart}>Add now </button>
                 </div>
               </div>
             </div>
