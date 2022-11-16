7335012d670d358aca883b35b3d5c85fa3c6ee19
fmiguezo <miguezflorencia@gmail.com>
2022-11-16 14:43:20 -0300

    :zap: cleaned up User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useUserContext } from "../context/userContext";
 import Cart from "./Cart";
 import PurchaseForm from "./PurchaseForm";
