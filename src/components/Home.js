d12bd15240175231d6ef5d0721a168f0fd8051c8
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-11-18 15:56:40 -0300

    conflict resolved

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,5 @@
 import ItemListContainter from "./ItemListContainter";
 
-<<<<<<< HEAD
-export const Home = () =>{
-
-    return(
-        <>
-            <ItemListContainter/>
-        </>
-    )
-}
-=======
 export const Home = () => {
   return (
     <div>
@@ -17,4 +7,3 @@ export const Home = () => {
     </div>
   );
 };
->>>>>>> 67f25ea387f08e3f48f311ff39cdf0821a9f802e
